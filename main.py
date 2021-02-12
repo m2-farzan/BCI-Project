6d16aeeb7ca019fd9c6694e6f59a0f135dec63b1
m2-farzan <m2_farzan@yahoo.com>
2021-02-13 00:58:48 +0330

    Tidy up

--- main.py
+++ main.py
@@ -1,147 +1,86 @@
-import numpy as np
-from scipy.signal import hilbert
-from sklearn.base import BaseEstimator, TransformerMixin
+"""By Erfan Mahmoodtabar & Mohammad Farzan."""
 
-
-class Flatten(TransformerMixin, BaseEstimator):
-    def __init__(self):
-        pass
-
-    def fit(self, X, y):
-        return self
-
-    def transform(self, X):
-        return np.reshape(X, (X.shape[0], X.shape[1] * X.shape[2]))
-
-
+# Our modules:
+from cnn import CNN1
 from preprocessing import FBCSP_Select 
 from preprocessing import Hilbert
 from preprocessing import Resample
 
-"""
-=========================
-FilterBank CSP versus CSP
-=========================
-
-This Example show a comparison of CSP versus FilterBank CSP on the
-very popular dataset 2a from the BCI competition IV.
-"""
-# Authors: Alexandre Barachant <alexandre.barachant@gmail.com>
-#
-# License: BSD (3-clause)
-
-import moabb
+# Library modules:
 import matplotlib.pyplot as plt
-import seaborn as sns
+import moabb
+from moabb.datasets import BNCI2014001
+from moabb.paradigms import MotorImagery
+from moabb.evaluations import CrossSessionEvaluation
+from mne.decoding import CSP
+import numpy as np
 import pandas as pd
-
+import seaborn as sns
 from sklearn.discriminant_analysis import LinearDiscriminantAnalysis as LDA
 from sklearn.pipeline import make_pipeline
 
-from mne.decoding import CSP
 
-from moabb.datasets import BNCI2014001
-from moabb.paradigms import MotorImagery, FilterBankMotorImagery
-from moabb.evaluations import CrossSessionEvaluation
-from moabb.pipelines.utils import FilterBank
-
-from cnn import CNN1
-
+# Definitions
 moabb.set_log_level('info')
+N_SUBJECTS = 2
 
-##############################################################################
-# Create pipelines
-# ----------------
-#
-# The CSP implementation from MNE is used. We selected 8 CSP components, as
-# usually done in the litterature.
-#
-# The second pipeline is the filter bank CSP. We use the FilterBank object
-# with a CSP estimator. We set up the CSP to 4 components, to compensate for
-# the higher dimensionality.
-#
-# The two pipelines will be applied on two different paradigm, so they have
-# their own dict.
+pipelines = {
+    'CSP + LDA': make_pipeline(
+        CSP(n_components=8),
+        LDA()),
+    'Sakhavi (2018)': make_pipeline(
+        FBCSP_Select(),
+        Hilbert(),
+        Resample(),
+        CNN1()),
+}
 
-pipelines = {}
-pipelines['CSP + LDA'] = make_pipeline(CSP(n_components=8),
-                                       LDA())
 
-pipelines_fb = {}
-pipelines_fb['FBCSP + LDA'] = make_pipeline(FBCSP_Select(),
-                                            Hilbert(),
-                                            Resample(),
-                                            CNN1()
-                                            )
-
-##############################################################################
-# Evaluation
-# ----------
-#
-# Since two different preprocessing will be applied, we have two different
-# paradigm objects. We have to make sure their filter matchs so the comparison
-# will be fair.
-#
-# The first one is a standard `LeftRightImagery` with a 8 to 35 Hz broadband
-# filter.
-#
-# The second is a `FilterBankLeftRightImagery` with a bank of 6 filter, ranging
-# from 8 to 35 Hz.
-
-# Because this is being auto-generated we only use 2 subjects
+# Load dataset
 dataset = BNCI2014001()
-dataset.subject_list = dataset.subject_list[:2]
+dataset.subject_list = dataset.subject_list[:N_SUBJECTS]
 datasets = [dataset]
 overwrite = True  # set to True if we want to overwrite cached results
 
-# broadband filters
-fmin = 8
-fmax = 35
-paradigm = MotorImagery(fmin=fmin, fmax=fmax, n_classes=4)
-evaluation = CrossSessionEvaluation(paradigm=paradigm, datasets=datasets,
-                                    suffix='examples', overwrite=overwrite)
-results = evaluation.process(pipelines)
 
-# bank of 6 filter, by 4 Hz increment
-paradigm = MotorImagery(n_classes=4)
-evaluation = CrossSessionEvaluation(paradigm=paradigm, datasets=datasets,
-                                    suffix='examples', overwrite=overwrite)
-results_fb = evaluation.process(pipelines_fb)
-
-###############################################################################
-# After processing the two, we simply concatenate the results.
-
-results = pd.concat([results, results_fb])
+# Evaluate methods
+results = {}
+for name, pipeline in pipelines.items():
+    print("Evaluating pipeline: " + name)
+    paradigm = MotorImagery(fmin=8, fmax=35, n_classes=4)
+    evaluation = CrossSessionEvaluation(paradigm=paradigm, datasets=datasets,
+                                        suffix='examples', overwrite=overwrite)
+    results[name] = evaluation.process({name: pipeline})
 
 
-##############################################################################
-# Plot Results
-# ----------------
-#
-# Here we plot the results via normal methods. We the first plot is a pointplot
-# with the average performance of each pipeline across session and subjects.
-# The second plot is a paired scatter plot. Each point representing the score
-# of a single session. An algorithm will outperforms another is most of the
-# points are in its quadrant.
+# Visualize
+results = pd.concat(list(results.values()))
 
-fig, axes = plt.subplots(1, 2, figsize=[8, 4], sharey=True)
-
-sns.stripplot(data=results, y='score', x='pipeline', ax=axes[0], jitter=True,
+## Bar chart
+fig = plt.figure(figsize=[8, 4])
+sns.stripplot(data=results, y='score', x='pipeline', jitter=True,
               alpha=.5, zorder=1, palette="Set1")
-sns.pointplot(data=results, y='score', x='pipeline', ax=axes[0],
+sns.pointplot(data=results, y='score', x='pipeline',
               zorder=1, palette="Set1")
+plt.ylabel('Score')
+plt.ylim([0.5, 1])
+plt.tight_layout()
+fig.savefig("fig-bar.png", dpi=150)
 
-axes[0].set_ylabel('ROC AUC')
-axes[0].set_ylim(0.5, 1)
+## Paired plots
+for i in range(len(pipelines)):
+    for j in range(i+1, len(pipelines)):
+        name_i = list(pipelines.keys())[i]
+        name_j = list(pipelines.keys())[j]
+    
+        fig = plt.figure(figsize=[6, 4])
+        paired = results.pivot_table(values='score', columns='pipeline',
+                                     index=['subject', 'session'])
+        paired = paired.reset_index()
 
-# paired plot
-paired = results.pivot_table(values='score', columns='pipeline',
-                             index=['subject', 'session'])
-paired = paired.reset_index()
-
-sns.regplot(data=paired, y='FBCSP + LDA', x='CSP + LDA', ax=axes[1],
-            fit_reg=False)
-axes[1].plot([0, 1], [0, 1], ls='--', c='k')
-axes[1].set_xlim(0.5, 1)
-
-plt.show()
+        sns.regplot(data=paired, y=name_i, x=name_j,
+                    fit_reg=False)
+        plt.plot([0, 1], [0, 1], ls='--', c='k')
+        plt.xlim([0.3, 1])
+        plt.tight_layout()
+        fig.savefig("fig-reg-%d-vs-%d.png" % (i, j), dpi=150)
